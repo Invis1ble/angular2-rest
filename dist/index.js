2bf810d615a24bce4b8f352191bd80f21a7efb0a
Invis1ble <invisiblexman2010@gmail.com>
2016-12-12 09:56:26 +0300

    Improved barrel, closes #2

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,14 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require('./rest.module'));
+__export(require('./rest.service'));
 __export(require('./decorators/resource'));
+__export(require('./decorators/action'));
+__export(require('./decorators/body'));
+__export(require('./decorators/parameter'));
+__export(require('./decorators/query'));
+__export(require('./decorators/query'));
+__export(require('./method-resolver'));
+__export(require('./path-generator'));
+__export(require('./headers'));
 //# sourceMappingURL=index.js.map
